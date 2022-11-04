39eb6c0f7c5fb2e17a967d2a3fc8ce2712b71783
pozerinhooo <jkob.matras@gmail.com>
2022-11-04 18:43:47 +0100

    Adapted token intercepting and model attributes for new authorization functionality

--- src/app/model/auth/login-payload-response.ts
+++ src/app/model/auth/login-payload-response.ts
@@ -1,6 +1,5 @@
 export class LoginPayloadResponse {
   constructor(public authenticationToken: string,
-              public refreshToken: string,
               public expiresAt: string,
               public username: string) {
   }
